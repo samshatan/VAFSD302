87f780fdc4a09a1563f8f42d02b84a90435fa394
samshatan <sameermaurya97044@gmail.com>
2024-10-21 09:43:53 +0530

    update

--- mobile/array.py
+++
@@ -1,5 +0,0 @@
-import numpy as n
-a=n.array([1,3,2])
-print("array with rank 1",a)
-b=n.array([[1,2,3],[4,5,6]])
-print("array with rank 2",b)
--- mobile/traversing.py
+++
@@ -1,8 +0,0 @@
-a="ABES ENGINEERING"
-print(a[5:10])
-print(a[:10])
-print(a[5:])
-print(a[:])
-print(a[5::2])
-print(a[-8:-3])
-print(a[::-1])
